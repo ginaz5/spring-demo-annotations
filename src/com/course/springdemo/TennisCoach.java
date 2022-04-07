fad4442c9be7b8ad8c9c062430d78827604ce689
Gina Lin <ginalin@alum.ccu.edu.tw>
2022-04-07 23:47:46 +0800

    setter and field injections

--- src/com/course/springdemo/TennisCoach.java
+++ src/com/course/springdemo/TennisCoach.java
@@ -6,12 +6,28 @@ import org.springframework.stereotype.Component;
 @Component // the default bean id will be tennisCoach
 public class TennisCoach implements Coach {
 
+    @Autowired
     private FortuneService fortuneService;
 
+    // define a default constructor
+    public TennisCoach() {
+        System.out.println(">> TennisCoach: default constructor");
+    }
+
+    /* define a setter method
     @Autowired
+    public void anyMethodName(FortuneService fortuneService) {
+        System.out.println(">> TennisCoach: inside of anyMethodName");
+        this.fortuneService = fortuneService;
+    }
+    */
+
+    /*
+    @Autowired // inject constructor
     public TennisCoach(FortuneService theFortuneService) {
         this.fortuneService = theFortuneService;
     }
+    */
 
     @Override
     public String getDailyWorkout() {
