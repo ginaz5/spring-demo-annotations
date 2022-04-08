497e1024916a78398a1477c66f239516d69bcc5c
Gina Lin <ginalin@alum.ccu.edu.tw>
2022-04-09 00:08:33 +0800

    Use annoation @Qualifier

--- src/com/course/springdemo/TennisCoach.java
+++ src/com/course/springdemo/TennisCoach.java
@@ -1,12 +1,14 @@
 package com.course.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component // the default bean id will be tennisCoach
 public class TennisCoach implements Coach {
 
     @Autowired
+    @Qualifier("happyFortuneService")
     private FortuneService fortuneService;
 
     // define a default constructor
